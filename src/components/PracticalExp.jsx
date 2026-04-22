78977cf231574638c5400f3752320e2e1dfcb418
Shalama <shalamafadillaa@gmail.com>
2026-04-22 11:52:39 +0700

    Create PracticalExp component

---
+++ src/components/PracticalExp.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Input from "./Input";
+
+export default function PracticalExp() {
+    const [company, setCompany] = useState('');
+    const [positionTitle, setPositionTitle] = useState('');
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+
+    function handleCompanyChange(e) {
+        setCompany(e.target.value);
+    }
+
+    function handlePositionTitleChange(e) {
+        setPositionTitle(e.target.value);
+    }
+
+    function handleStartDateChange(e) {
+        setStartDate(e.target.value);
+    }
+
+    function handleEndDateChange(e) {
+        setEndDate(e.target.value);
+    }
+
+    return (
+        <>
+            <Input label="Company's Name" value={company} onChange={handleCompanyChange} type='text'></Input>
+            <Input label="Position Title" value={positionTitle} onChange={handlePositionTitleChange} type='text'></Input>
+            <Input label="Start Date" value={startDate} onChange={handleStartDateChange} type='date'></Input>
+            <Input label="End Date" value={endDate} onChange={handleEndDateChange} type='date'></Input>
+        </>
+    )
+}
