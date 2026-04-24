fe182f954fef0e8f72a3a1be653e9b9c9aa09c4f
Shalama <shalamafadillaa@gmail.com>
2026-04-24 11:42:23 +0700

    Add header element inside Header component return

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,11 @@
 export default function Header({name}) {
-    return <h1>
-        {name}'s CV Application
-    </h1>
+    return (
+        <>
+            <header>
+                <h1>
+                    {name}'s CV Application
+                </h1>
+            </header>
+        </>
+    )
 }
